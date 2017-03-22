bb6958e7add29dce1ebfd3aa455fc8db6af5a6c3
marcelo <marcelobbff@gmail.com>
2017-03-21 21:50:29 -0300

    adicionado assets

---
+++ cmake-build-debug/CMakeFiles/trabalho_idj.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabalho_idj.dir/Game.cpp.o"
+  "CMakeFiles/trabalho_idj.dir/main.cpp.o"
+  "CMakeFiles/trabalho_idj.dir/Sprite.cpp.o"
+  "CMakeFiles/trabalho_idj.dir/State.cpp.o"
+  "trabalho_idj.pdb"
+  "trabalho_idj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trabalho_idj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
