6185a58f941eabbc8cdaaea9fcbd541ac099eb68
marcelo <marcelobbff@gmail.com>
2017-04-04 23:33:13 -0300

    started T3 configuring tileset and tilemap

--- TileMap.cpp
+++ TileMap.cpp
@@ -6,6 +6,17 @@ void TileMap::Load(string file){
 
 }
 
+void TileMap::SetTileSet(TileSet* tileSet){
+
+}
+int& TileMap::At(int x, int y, int z = 0){
+	return 0;
+}
+
+void TileMap::RenderLayer(int layer, int cameraX = 0, int cameraY = 0){
+	
+}
+
 TileMap::~TileMap(){
 
 }
